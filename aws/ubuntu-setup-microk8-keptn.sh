299dca91b035445b5fd3010f2a18af942f770781
Sergio Hinojosa <sergio.hinojosa@dynatrace.com>
2020-03-25 16:37:56 +0100

    zwis. stand keptn with ssl

--- aws/ubuntu-setup-microk8-keptn.sh
+++ aws/ubuntu-setup-microk8-keptn.sh
@@ -1,8 +1,7 @@
 #!/bin/bash -x
 ## Commands for Ubuntu Server 18.04 LTS (HVM), SSD Volume Type
 ## These script will install the following components:
-## Microkubernetes
-
+## Microkubernetes 1.15, Keptn 6.1 with Istio 1.5 and Helm 1.2, the OneAgent and an ActiveGate
 
 # Log duration
 SECONDS=0
@@ -13,18 +12,16 @@ export PAASTOKEN=
 export APITOKEN=
 LOGFILE='/tmp/install.txt'
 
-
 ##Create installer Logfile
 printf "\n\n***** Init Installation ***\n" >> $LOGFILE 2>&1 
 { date ; apt update; whoami ; echo Setting up microkubernetes with keptn and oneagent for tenant: $TENANT with Api-Token: $APITOKEN and PaaS-Token: $PAASTOKEN; } >> $LOGFILE ; chmod 777 $LOGFILE
-
 
 printf "\n\n***** Update and install docker***\n" >> $LOGFILE 2>&1 
 { apt install docker.io -y ;\ 
  service docker start ;\
  usermod -a -G docker ubuntu ;} >> $LOGFILE 2>&1
 
-# Install Kubernetes 1.16
+# Install Kubernetes 1.15
 printf "\n\n***** Install Microk8s 1.15 and allow the KubeApiServer to run priviledged pods***\n" >> $LOGFILE 2>&1 
 { snap install microk8s --channel=1.15/stable --classic ;\
 bash -c "echo \"--allow-privileged=true\" >> /var/snap/microk8s/current/args/kube-apiserver" ;} >> $LOGFILE 2>&1
@@ -52,46 +49,56 @@ alias vaml='vi -c \"set syntax:yaml\" -'
 alias vson='vi -c \"set syntax:json\" -' 
 alias pg='ps -aux | grep' " > /root/.bash_aliases
 
-#Copy Aliases
+# Copy Aliases
 cp /root/.bash_aliases /home/ubuntu/.bash_aliases
 cp /root/.bash_aliases /home/dynatrace/.bash_aliases
 
 # Add alias to Kubectl
 snap alias microk8s.kubectl kubectl 
 
+# Add Snap to the system wide environment. 
+sed -i 's~/usr/bin:~/usr/bin:/snap/bin:~g' /etc/environment
+
 #Start Micro Enable Default Modules as Ubuntu
 # Passing the commands to ubuntu since it has microk8s in its path and also does not have password enabled otherwise the install will fail
 { echo "\n\n***** Starting microk8s *****\n" ;\
-sudo -H -u ubuntu bash -c 'microk8s.start && microk8s.enable dns storage ingress istio dashboard' ;} >> $LOGFILE 2>&1
+sudo -H -u dynatrace bash -c 'microk8s.start && microk8s.enable dns storage ingress dashboard' ;} >> $LOGFILE 2>&1
 
+# Copy the Workshop from Github and unpack them
+git clone --branch 0.6.1 https://github.com/acm-workshops/keptn-workshop.git /home/dynatrace/keptn-workshop  --single-branch
+
+# TODO Dont clone this repo, KISS.
+# is this needed? I can add a subfolder and merge?
+git clone --branch 0.6.1 https://github.com/keptn/examples.git /home/dynatrace/examples --single-branch
+
+# TODO Dont clone this repo, KISS.
 # Download YAML files from Github and unpack them
-git clone https://github.com/sergiohinojosa/kubernetes-deepdive /home/ubuntu/kubernetes  
+git clone https://github.com/sergiohinojosa/kubernetes-deepdive /home/dynatrace/kubernetes  
 
-# Set permisions
-chown ubuntu:ubuntu -R /home/ubuntu/kubernetes/
+# Change owner of cloned folders
+chown dynatrace:dynatrace -R /home/dynatrace/
+
+# Installation of istio 1.5 
+{  printf "\n\n*****Install istio 1.5 into /opt and add it to user/local/bin ***** \n" ;\
+curl -L https://istio.io/downloadIstio | sh - ;\
+mv istio-1.5.0 /opt/istio-1.5.0 ;\
+chmod +x -R /opt/istio-1.5.0/ ;\
+ln -s /opt/istio-1.5.0/bin/istioctl /usr/local/bin/istioctl ;\
+sudo -H -u dynatrace bash -c "echo 'y' | istioctl manifest apply" ;} >> $LOGFILE 2>&1
 
 
-git clone --branch 0.6.1 https://github.com/keptn/examples.git /home/ubuntu/examples --single-branch
-chown ubuntu:ubuntu -R /home/ubuntu/examples/
-
-{ printf "\n\n***** Waiting for pods to start.... we sleep for 1 minute before the installation of Keptn *****\n" ;\
+{ printf "\n\n***** Waiting for pods to start.... we sleep for 1 minute *****\n" ;\
 sleep 1m ;} >> $LOGFILE 2>&1
 
 
 # Allow access to K8 Dashboard withouth login
 # Create ingresses
-# { sudo -H -u ubuntu bash -c 'kubectl apply -f /home/dynatrace/kubernetes/k8-dashboard/skip-login-in-k8-dashboard.yaml' ;\
-# sudo -H -u ubuntu bash -c 'cd /home/dynatrace/kubernetes/keptn/setup && sh route_istio.sh' ;} >> $LOGFILE 2>&1
+# { sudo -H -u dynatrace bash -c 'kubectl apply -f /home/dynatrace/kubernetes/k8-dashboard/skip-login-in-k8-dashboard.yaml' ;\
+# sudo -H -u dynatrace bash -c 'cd /home/dynatrace/kubernetes/keptn/setup && sh route_istio.sh' ;} >> $LOGFILE 2>&1
 
 ## Installation of Active Gate
-## { wget -nv -O activegate.sh "$TENANT/api/v1/deployment/installer/gateway/unix/latest?Api-Token=$PAASTOKEN&arch=x86&flavor=default"
-## sh activegate.sh ;} >> $LOGFILE 2>&1 
-
-## Installation of Keptn
-{  printf "\n\n***** Downloading Keptn ***** \n" ; wget -q -O keptn.tar https://github.com/keptn/keptn/releases/download/0.6.1/0.6.1_keptn-linux.tar ;\
-tar -xvf keptn.tar ;\
-chmod +x keptn ;\
-mv keptn /usr/local/bin/keptn ;}   >> $LOGFILE 2>&1
+{ wget -nv -O activegate.sh "$TENANT/api/v1/deployment/installer/gateway/unix/latest?Api-Token=$PAASTOKEN&arch=x86&flavor=default"
+sh activegate.sh ;} >> $LOGFILE 2>&1 
 
 # Installation of Helm Client
 {  printf "\n\n***** Downloading HELM *****\n" ; wget -O getHelm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get ;\
@@ -99,77 +106,51 @@ chmod +x getHelm.sh ;\
 ./getHelm.sh -v v2.12.3 ;\
 helm init ;}   >> $LOGFILE 2>&1
 
-printf "\n\n***** Create Istio NS and route traffic to istio ingress-gateway ****\n"
-{ sudo -H -u ubuntu bash -c 'kubectl create ns istio-system' ;\
-sudo -H -u ubuntu bash -c 'cd /home/ubuntu/kubernetes/keptn/setup && sh route_istio.sh' ;} >> $LOGFILE 2>&1
+## Installation of Keptn
+{  printf "\n\n***** Downloading KEPTN ***** \n" ; wget -q -O keptn.tar https://github.com/keptn/keptn/releases/download/0.6.1/0.6.1_keptn-linux.tar ;\
+tar -xvf keptn.tar ;\
+chmod +x keptn ;\
+mv keptn /usr/local/bin/keptn ;}   >> $LOGFILE 2>&1
+
+
+printf "\n\n***** Install CertManager ****\n"
+{ sudo -H -u dynatrace bash -c 'kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v0.14.0/cert-manager.yaml' ;} >> $LOGFILE 2>&1
+
+
+printf "\n\n***** Route Traffic to IstioGateway and Create SSL certificates for Istio Endpoints ****\n"
+{ sudo -H -u dynatrace bash -c 'cd /home/dynatrace/keptn-workshop/setup && sh expose-ssl-istio.sh' ;} >> $LOGFILE 2>&1
 
 {  printf "\n\n*****Configure Public Domain for Microk8s  ***** \n" ;\
 export PUBLIC_IP=$(curl -s ifconfig.me) ;\
 PUBLIC_IP_AS_DOM=$(echo $PUBLIC_IP | sed 's~\.~-~g') ;\
 export DOMAIN="${PUBLIC_IP_AS_DOM}.nip.io" ;\
 printf "Public DNS: $DOMAIN"
-sudo -H -u ubuntu bash -c "kubectl create configmap keptn-domain --from-literal=domain=$DOMAIN" ;} >> $LOGFILE 2>&1
-
-##### Manuall ab hier!!
+sudo -H -u dynatrace bash -c "kubectl create configmap keptn-domain --from-literal=domain=$DOMAIN" ;} >> $LOGFILE 2>&1
 
 
-{ printf "\n\n***** Kick installation of Keptn *****\n" ;\
-sudo -H -u ubuntu bash -c 'echo 'y' | keptn install --platform=kubernetes --istio-install-option=Overwrite --gateway=LoadBalancer --keptn-installer-image=shinojosa/keptninstaller:6.1.customdomain' ;} >> $LOGFILE 2>&1
+{ printf "\n\n***** Install Keptn *****\n" ;\
+sudo -H -u dynatrace bash -c 'echo 'y' | keptn install --platform=kubernetes --istio-install-option=Reuse --gateway=LoadBalancer --keptn-installer-image=shinojosa/keptninstaller:6.1.customdomain' ;} >> $LOGFILE 2>&1
 
+# Authorize keptn
+printf "\nFor authorizing Keptn type
+keptn auth --endpoint=https://api.keptn.$(kubectl get cm -n keptn keptn-domain -ojsonpath={.data.app_domain}) --api-token=$(kubectl get secret keptn-api-token -n keptn -ojsonpath={.data.keptn-api-token} | base64 --decode)\n"
 
 { printf "\n\n***** Waiting 1 minutes for keptn to initialize. *****\n" ;\
 sleep 1m ;} >> $LOGFILE 2>&1
 
-# NoteToSelf, in micro1.16 the nginx has an own namespace. Why do 1.15 need two pods? who knows....
-{ printf "\n\n***** After Keptn installation recycle ingress NGINX Pods so the routing to istio works *****\n" ;\
-sudo -H -u ubuntu bash -c 'kubectl delete po --all' ;} >> $LOGFILE 2>&1
-
-{ printf "\n\n***** Wait another minute for NGINX pods to initialize. *****\n" ;\
-sleep 1m ;} >> $LOGFILE 2>&1
-
-# Allow unencrypted password via SSH for login
-# Restart the SSHD Service
-{ printf "\n\n***** Allow Password authentication and restarting SSH service *****\n" ;\
-sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config ;\
-service sshd restart ;} >> $LOGFILE 2>&1
+# Remove after test NoteToSelf, in micro1.16 the nginx has an own namespace. Why do 1.15 need two pods? who knows....
+# { printf "\n\n***** After Keptn installation recycle ingress NGINX Pods so the routing to istio works *****\n" ;\
+# sudo -H -u dynatrace bash -c 'kubectl delete po --all' ;} >> $LOGFILE 2>&1
 
 
-# Now lets install Dynatrace
-# Create secret
-# Deploy Dynatrace service
-# Deploy SLI Service 
-# Deploy Operator via Keptn 
-# prefix="https://";string="https://tetete.com/iaoaia";foo=${string#"$prefix"}
-{ printf "\n\n***** Installing and configuring Dynatrace on the Cluster *****\n\n" ;\
-PROTOCOL="https://";CLEAN_URL=${TENANT#"$PROTOCOL"} ; printf "This is the Tenant:$CLEAN_URL"  ;\
-sudo -H -u ubuntu bash -c "kubectl -n keptn create secret generic dynatrace --from-literal=\"DT_TENANT=$CLEAN_URL\" --from-literal=\"DT_API_TOKEN=$APITOKEN\"  --from-literal=\"DT_PAAS_TOKEN=$PAASTOKEN\"" ;\
-sudo -H -u ubuntu bash -c "kubectl apply -f https://raw.githubusercontent.com/keptn-contrib/dynatrace-service/0.6.1/deploy/manifests/dynatrace-service/dynatrace-service.yaml" ;\
-sudo -H -u ubuntu bash -c "kubectl apply -f https://raw.githubusercontent.com/keptn-contrib/dynatrace-sli-service/0.3.0/deploy/service.yaml" ;\
-sudo -H -u ubuntu bash -c "keptn configure monitoring dynatrace" ;} >> $LOGFILE 2>&1
+# { printf "\n\n***** Wait another minute for NGINX pods to initialize. *****\n" ;\
+# sleep 1m ;} >> $LOGFILE 2>&1
 
-# Expose bridge via VS /home/ubuntu/kubernetes/keptn/
-{ printf "\n\n*****  Expose Bridge via VS  *****\n\n" ;\
-DOMAIN=$(sudo -H -u ubuntu bash -c "kubectl get cm -n keptn keptn-domain -ojsonpath={.data.app_domain}") ;\
-sudo -H -u ubuntu bash -c "cat /home/ubuntu/kubernetes/keptn/expose-bridge/manifests/bridge.yaml | sed 's~domain.placeholder~'\"$DOMAIN\"'~' > /home/ubuntu/kubernetes/keptn/expose-bridge/manifests/gen/bridge.yaml" ;\
-sudo -H -u ubuntu bash -c "kubectl apply -f /home/ubuntu/kubernetes/keptn/expose-bridge/manifests/gen/bridge.yaml" ;}  >> $LOGFILE 2>&1
+# Install OA
 
-# Unleash Server? 
-{ printf "\n\n*****  Deploy Unleash-Server  *****\n\n" ;\
-sudo -H -u ubuntu bash -c "cd /home/ubuntu/examples/unleash-server/ && sh /home/ubuntu/kubernetes/keptn/setup/deploy_unleashserver.sh" ;} >> $LOGFILE 2>&1
+# Expose Bridge
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Unleash
 
 # Installation finish, print time.
 DURATION=$SECONDS
