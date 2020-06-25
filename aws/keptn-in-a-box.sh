3d75bdf404693da64908a9d0439436c8f407002e
Sergio Hinojosa <sergio.hinojosa@dynatrace.com>
2020-06-25 09:28:37 +0200

    updating keptn in a box

--- aws/keptn-in-a-box.sh
+++ aws/keptn-in-a-box.sh
@@ -1,23 +1,42 @@
 #!/bin/bash
-## Ubuntu Server 20.04 LTS or 18.04 LTS  (HVM) for full functionality size 2xlarge 
-## Microkubernetes 1.15, Keptn 6.1 with Istio 1.5, Helm 1.2, Docker, Registry, Dynatrace OneAgent and Dynatrace ActiveGate
+# =========================================================
+#      -------- Keptn-in-a-Box -------                    #
+# Script for installing Keptn in an Ubuntu Server LTS     #
+# (20 or 18). Installed components are but not limited to:#
+# Microk8s, Keptn, Istio, Helm, Docker, Docker Registry   #
+# Jenkins, Dynatrace OneAgent, Dynatrace ActiveGate,      #
+# Unleash, KeptnExamples                                  #
+#                                                         #
+# This file is the main installation process. In here you #
+# define the installationBundles and its functions.       #
+# The definition of variables and different versions are  #
+# defined here.                                           #
+#                                                         #
+# 'functions.sh' is where the functions are defined and   #
+# will be loaded into this shell upon execution.          #
+# controlled via boolean flags.                           #
+#                                                         #
+# An installationBundle contains a set of multiple ins-   #
+# tallation functions.                                    #
+# =========================================================
 
-## ----  Define variables ----
+# ==================================================
+#      ----- Variables Definitions -----           #
+# ==================================================
 LOGFILE='/tmp/install.log'
+touch $LOGFILE
 chmod 775 $LOGFILE
 pipe_log=true
 
-# The installation will look for this file locally, if not found it will pull it form github.
+# - The installation will look for this file locally, if not found it will pull it form github.
 FUNCTIONS_FILE='functions.sh'
 
-# The user to run the commands from. Will be overwritten when executing this shell with sudo 
-USER="ubuntu"
+# ---- Workshop User  ---- 
+# The flag 'create_workshop_user'=true is per default set to false. If it's set to to it'll clone the home directory from USER and allow SSH login with the given text password )
+NEWUSER="student"
+NEWPWD="secr3t"
 
-# create_workshop_user=true (will clone the home directory from USER and allow SSH login with text password )
-NEWPWD="dynatrace"
-NEWUSER="dynatrace"
-
-# ****  Define Dynatrace Environment **** 
+# ---- Define Dynatrace Environment ---- 
 # Sample: https://{your-domain}/e/{your-environment-id} for managed or https://{your-environment-id}.live.dynatrace.com for SaaS
 TENANT=
 PAASTOKEN=
@@ -25,22 +44,17 @@ APITOKEN=
 
 # Set your custom domain e.g for an internal machine like 192.168.0.1.nip.io
 # So Keptn and all other services are routed and exposed properly via the Ingress Gateway
-# if no DOMAIN is setted, the public IP of the machine will be converted to a magic nip.io domain   
+# if no DOMAIN is setted, the public IP of the machine will be converted to a magic nip.io domain 
+# ---- Define your Domain ----   
 DOMAIN=
 
-# **** Installation Versions **** 
-ISTIO_VERSION=1.5.1
-HELM_VERSION=2.12.3
-CERTMANAGER_VERSION=0.14.0
-KEPTN_VERSION=0.6.1
-KEPTN_DT_SERVICE_VERSION=0.6.2
-KEPTN_DT_SLI_SERVICE_VERSION=0.3.1
-KEPTN_EXAMPLES_BRANCH=0.6.1
-TEASER_IMAGE="shinojosa/nginxacm"
-KEPTN_BRIDGE_IMAGE="keptn/bridge2:20200326.0744"
-MICROK8S_CHANNEL="1.15/stable"
+# ==================================================
+#      ----- Functions Location -----                #
+# ==================================================
+FUNCTIONS_FILE_REPO="https://raw.githubusercontent.com/keptn-sandbox/keptn-in-a-box/master/functions.sh"
 
-## ----  Write all to the logfile ----
+
+## ----  Write all output to the logfile ----
 if [ "$pipe_log" = true ] ; then
   echo "Piping all output to logfile $LOGFILE"
   echo "Type 'less +F $LOGFILE' for viewing the output of installation on realtime"
@@ -62,10 +76,10 @@ fi
 
 # Load functions after defining the variables & versions
 if [ -f "$FUNCTIONS_FILE" ]; then
-    echo "The functions file $FUNCTIONS_FILE exists locally, loading functions from it."
+    echo "The functions file $FUNCTIONS_FILE exists locally, loading functions from it. (dev)"
 else 
     echo "The functions file $FUNCTIONS_FILE does not exist, getting it from github."
-    curl -o functions.sh https://raw.githubusercontent.com/keptn-sandbox/keptn-in-a-box/master/functions.sh
+    curl -o functions.sh $FUNCTIONS_FILE_REPO
 fi
 
 # Comfortable function for setting the sudo user.
@@ -82,22 +96,45 @@ shopt -s expand_aliases
 # --- Loading the functions in the current shell
 source $FUNCTIONS_FILE
 
+# ==================================================
+#      ----- Override Components Versions -----    #
+# ==================================================
+#MICROK8S_CHANNEL="1.15/stable"
+#KEPTN_IN_A_BOX_DIR="~/keptn-in-a-box"
+#KEPTN_EXAMPLES_DIR="~/examples"
+#KEPTN_IN_A_BOX_REPO="https://github.com/keptn-sandbox/keptn-in-a-box"
+# ==================================================
+#    ----- Select your installation Bundle -----   #
+# ==================================================
+# Uncomment for installing only Keptn 
+# installationBundleKeptnOnly
 
-# --- Enable the installation Modules --- 
-# Uncomment for installing the Default 
-#installationModulesDefault
-installationModulesFull
-# - Uncomment below for installing the minimal setup
-#installationModulesMinimal
+# - Comment out if selecting another bundle
+installationBundleDemo
+
+#installationBundleKeptnQualityGates
+
+# - Uncomment for installing Keptn-in-a-Box for Workshops
+# installationBundleWorkshop
 
 # - Uncomment below for installing all features
-#installationModulesFull
+#installationBundleAll
 
+# - Uncomment below for installing a PerformanceAsAService Box
+#installationBundlePerformanceAsAService
+
+# ==================================================
+# ---- Enable or Disable specific functions -----  #
+# ==================================================
 # -- Override a module like for example verbose output of all commands
-#verbose_mode=true
+#verbose_mode=false
+
 # -- or install cert manager 
 #certmanager_install=true
 #certmanager_enable=true
+#create_workshop_user=true
 
-# *** Do Installation 
+# ==================================================
+#  ----- Call the Installation Function -----      #
+# ==================================================
 doInstallation
